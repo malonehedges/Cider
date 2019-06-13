a155e34f534f161ff26fdaa99801776d4c2fa78f
Malone Hedges <malonehedges@users.noreply.github.com>
2019-06-13 15:10:39 -0400

    Try PlayParameters as a dictionary

--- Sources/Cider/ResourceObjects/PlayParameters.swift
+++ Sources/Cider/ResourceObjects/PlayParameters.swift
@@ -9,10 +9,12 @@
 import Foundation
 
 /// An object that represents play parameters for resources.
-public struct PlayParameters: Codable {
-    /// The ID of the content to use for playback
-    public let id: String
+public typealias PlayParameters = [String: String]
 
-    /// The kind of the content to use for playback
-    public let kind: String
-}
+//public struct PlayParameters: Codable {
+//    /// The ID of the content to use for playback
+//    public let id: String
+//
+//    /// The kind of the content to use for playback
+//    public let kind: String
+//}
