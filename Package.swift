350e0a0de42ac23b8c6e57212d2fcc6138d1b03a
Malone Hedges <me@malone.io>
2021-02-03 19:58:49 -0800

    update package

--- Package.swift
+++ Package.swift
@@ -1,5 +1,19 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-    name: "Cider"
+    name: "Cider",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(
+            name: "Cider",
+            targets: ["Cider"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Cider",
+            dependencies: [])
+    ],
+    swiftLanguageVersions: [.version("5.0")]
 )
